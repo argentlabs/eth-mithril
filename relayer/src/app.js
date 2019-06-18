5c7119b0025563eb23c566ddc3bb9e9c1e4e6d80
Gerald <gergold@gmail.com>
2019-06-18 16:16:24 +0200

    add rate limit middleware

--- relayer/src/app.js
+++ relayer/src/app.js
@@ -1,11 +1,24 @@
 require('dotenv').config();
 
 const jayson = require('jayson');
+const express = require('express');
+const jsonParser = require('body-parser').json;
+const rateLimit = require("express-rate-limit");
+
 const Controller = require('./controller');
 
 const controller = new Controller(process.env.PRIVATE_KEY, process.env.PROVIDER_URL, process.env.MIXER_ADDRESS);
 
-const methods = {
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.set('trust proxy', 1);
+app.use(rateLimit({
+    windowMs: process.env.RATELIMIT_WINDOW || 15 * 60 * 1000,
+    max: process.env.RATELIMIT_MAX || 3
+}));
+
+const server = jayson.server({
     eth_sendTransaction: async (args, callback) => {
         try {
             const params = args[0];
@@ -15,8 +28,13 @@ const methods = {
             return callback({ code: -1, message: error.message });
         }
     }
-}
+});
 
-const port = process.env.PORT || 8080;
-const server = jayson.server(methods);
-server.http().listen(port);
+app.get('/health', (req, res) => {
+    res.json({ status: 'OK' })
+});
+
+app.use(jsonParser());
+app.use(server.middleware());
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
